54ef1b2e66bf8d179c654c5369dba1d948baea00
MichalKoscinski <michalkoscinski15@gmail.com>
2020-02-29 13:46:19 +0000

    HashRouter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Sections from './views/Sections/Sections'
 import { CSSTransition, SwitchTransition } from 'react-transition-group'
 import './addons/css/fadeTransition.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './views/Home/Home'
 
 
 function App() {
   return (
     <Router>
+      basename={"/"}
       <div className="App">
         <main>
           <Route render={({ location }) => (
