fb89b3a75a47b65b52bddb6685addb03fe4367b3
MichalKoscinski <michalkoscinski15@gmail.com>
2020-03-07 21:29:19 +0000

    changed text

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,17 +10,13 @@ const AboutMe = () => {
             </h1>
             <div className="description">
                 <p>
-                I am currently a second year student of Mathematics, Operational Research, Statistics and Economics (MORSE) at the University of Warwick. 
-                The coursework mostly includes mathematical methods applied in data science, as well as in game theory and algorithms.
-                
+                I am currently a second year student of Mathematics, Operational Research, Statistics and Economics (MORSE) at the University of Warwick. Through my coursework, I'm getting familiar mathematical methods applied in data science, as well as in game theory and algorithms.
                 </p>
                 <p>
-                My programming experience started with Java and R taught at Warwick. Later, I got interested in front-end development and started learning HTML, CSS and JavaScript.
-                
+                My programming experience started with Java and R that I've been taught at Warwick. Subsequently, I got interested in front-end development and started learning HTML, CSS and JavaScript.
                 </p>
                 <p>
-                Since then, I try to develop my projects in React given how many possibilities it offers. In the future I plan on developing my skills in data visualistaion to use my degree in practice.
-                
+                Since then, I'm trying to develop my projects in React - I'm fascinated by how many possibilities it offers. In the future I plan on developing my skills in data visualisation so that I can apply the skills I learn in my degree in the real world.
                 </p>
             </div>
         </section>
